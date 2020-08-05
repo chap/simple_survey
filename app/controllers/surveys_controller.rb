23f858330857efea9893770ff25ff7cfb459f5cd
Chap Ambrose <chapambrose@gmail.com>
2020-08-05 22:59:27 +0000

    rendering survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -38,14 +38,13 @@ class SurveysController < ApplicationController
     @survey = Survey.new
   end
 
-  # POST /surveys
-  # POST /surveys.json
   def create
+    params[:survey][:response][:housemates] = [] if params[:survey][:response][:housemates].nil?
     @survey = Survey.new(response: params[:survey][:response])
 
     respond_to do |format|
       if @survey.save
-        format.html { redirect_to @survey, notice: 'Survey was successfully created.' }
+        format.html { redirect_to short_survey_url(@survey), notice: "Response saved. Share the URL of this page: #{short_survey_url(@survey)}" }
         format.json { render :show, status: :created, location: @survey }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :surveys, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'surveys#new'
+
+  get '/:id', to: 'surveys#show', as: :short_survey
 end
